809a5fc6480b72104d433d792f0a97af7ce4489c
Faiz Firdaus <faiz.firdaus.2004@gmail.com>
2024-08-08 20:00:05 +0700

    verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en">
+      <head><meta name="google-site-verification" content="-h4ynCaHHpoWuOlLyDIhjqD7K8gARUoQe405WM7jxL0" /></head>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={inter.className} id="top">
         <Navbar />
