91af3abeac8e89acb8ea219307e60fa76ad49162
Faiz Firdaus <faiz.firdaus.2004@gmail.com>
2024-08-08 20:05:10 +0700

    meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en">
-      <head><meta name="google-site-verification" content="-h4ynCaHHpoWuOlLyDIhjqD7K8gARUoQe405WM7jxL0" /></head>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="-h4ynCaHHpoWuOlLyDIhjqD7K8gARUoQe405WM7jxL0"
+        />
+        <meta
+          name="description"
+          content="Faiz Firdaus Portfolio. Computer science student at UMass Amherst. Here are a list of my projects and experiences."
+        />
+        <meta />
+        <meta name="keywords" content="Faiz, Firdaus, Faiz Firdaus, Computer science, Software engineer, Research, Backend, Frontend, Internship, Intern, Student, Indonesia, United States, UMass Amherst, Massachusetts, Technology, Engineer, Design, CSS, React, Bpf, eBPF, Linux"></meta>
+      </head>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={inter.className} id="top">
         <Navbar />
