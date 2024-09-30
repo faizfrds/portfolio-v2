8775aa4c5caae45da439200233c5b3fb90e79972
faiz <faiz.firdaus.2004@gmail.com>
2024-09-30 19:51:58 -0400

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en">
+      <Analytics />
       <head>
         <meta
           name="google-site-verification"
