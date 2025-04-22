5a54fd7f6a4c037ec931c190f6af8b62a7adc602
faiz <faiz.firdaus.2004@gmail.com>
2025-04-21 22:23:35 -0400

    more pics

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,29 +3,33 @@ import Link from "next/link";
 import Image from "next/image"; // ✅ Import Next Image
 import { HiOutlineGlobeAlt, HiArrowLeft } from "react-icons/hi";
 
-const imageNames = [
-  "waterfall.png",
-  "bridge.png",
-  "wfall.png",
-  "canon.png",
-  "cool.png",
-  "DSC00421.png",
-  "hotel.png",
-  "DSC00294.png",
-  "tre.png",
-  "snowy.png",
-  "DSC01140.png",
-  "beach.png",
-  "car.jpg",
-  "car.JPG",
-  "DSC00215.jpg",
-  "DSC00226.jpeg",
-  "DSC00868.JPG",
-  "IMG_9349.jpg",
-  "yose.jpg",
-];
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
-export default function Gallery() {
+export const dynamic = "force-dynamic"; // needed if data changes often
+
+async function getImageNames() {
+  const s3 = new S3Client({
+    region: "us-east-2",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+  });
+
+  const command = new ListObjectsV2Command({ Bucket: "picturesforfaiz" });
+  const response = await s3.send(command);
+
+  const imageNames = response.Contents?.map((item) => {
+    if (item) return item.Key;
+  });
+  console.log(imageNames);
+  return imageNames;
+}
+
+export default async function Gallery() {
+  const imageNames = await getImageNames();
+  // console.log(imageNames)
+
   return (
     <main className="bg-gradient-to-b from-slate-950 to-teal-950 lg:px-20 py-10 justify-center flex">
       <div className="flex flex-wrap w-3/4 md:w-2/3 lg:mt-10 mt-20">
